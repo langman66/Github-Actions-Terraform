2f8b12e1ea14d19f6b53e19a4cfdc81a09127d77
langman66 <chris@chrislangston.com>
2025-08-15 11:35:33 -0400

    Update providers.tf
    Upgrade terraform azurerm version.

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -2,10 +2,11 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=2.46.0"
+      version = "~> 3.0"
     }
   }
 }
+
 provider "azurerm" {
   features {}
 }
