239eb65299894d7e039111886de7f4cd4174958c
langman66 <chris@chrislangston.com>
2025-08-17 09:02:20 -0400

    Update main.tf
    use simple main.tf file to create a resource group.

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,14 +6,21 @@ terraform {
     key                  = "dev.terraform.tfstate"
   }
 }
-module "RG" {
-  source   = "./modules/RG" #A
-  rgname   = var.rgname     #B
-  location = var.location
-}
-module "SA" {
-  source   = "./modules/StorageAccount"
-  sname    = var.sname
-  rgname   = var.rgname
-  location = var.location
+
+# module "RG" {
+#   source   = "./modules/RG" #A
+#   rgname   = var.rgname     #B
+#   location = var.location
+# }
+# module "SA" {
+#   source   = "./modules/StorageAccount"
+#   sname    = var.sname
+#   rgname   = var.rgname
+#   location = var.location
+# }
+
+
+resource "azurerm_resource_group" "cdl_tf_hos_eus" {
+  name     = "rg-cdl-tf-hos-eus"
+  location = "East US"
 }
